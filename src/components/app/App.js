5f271e1357624e9e7e81f142f8bf9cca52ac8b61
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-18 14:54:41 -0700

    Add redux to index, app, and auth components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { tryLoadUser } from '../auth/actions';
+import { getCheckedAuth } from '../auth/reducers';
 import Header from '../header/Header';
 import Auth from '../auth/Auth';
 
 class App extends Component {
+  static propTypes = {
+    tryLoadUser: PropTypes.func.isRequired,
+    checkedAuth: PropTypes.bool.isRequired
+  };
+
+  componentDidMount() {
+    this.props.tryLoadUser();
+  }
 
   render() {
-    
+    const { checkedAuth } = this.props;
     return (
       <Router>
         <div>
@@ -15,14 +27,16 @@ class App extends Component {
           </header>
           
           <main>
-            <Switch>
-              <Route exact path="/"/>
-              <Route path="/auth" component={Auth}/>
-              <Route exact path="/search"/>
-              <Route exact path="/about"/>
-              <Route exact path="/profile"/>
-              <Redirect to="/"/>
-            </Switch>
+            {checkedAuth &&
+              <Switch>
+                <Route exact path="/"/>
+                <Route path="/auth" component={Auth}/>
+                <Route exact path="/search"/>
+                <Route exact path="/about"/>
+                <Route exact path="/profile"/>
+                <Redirect to="/"/>
+              </Switch>
+            }
           </main>
     
         </div>
@@ -31,4 +45,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  state => ({ checkedAuth: getCheckedAuth(state) }),
+  { tryLoadUser }
+)(App);

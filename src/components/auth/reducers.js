0306e7f4daeb5ae31a45e872671967d19cc9469b
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-18 14:22:17 -0700

    Add user selector for getting user and verify test will pass

--- src/components/auth/reducers.js
+++ src/components/auth/reducers.js
@@ -2,6 +2,8 @@ export const USER_AUTH = 'USER_AUTH';
 export const LOGOUT = 'LOGOUT';
 export const CHECKED_AUTH = 'CHECKED_AUTH';
 
+export const getUser = state => state.user;
+
 export function user(state = null, { type, payload }) {
   switch(type) {
     case USER_AUTH:

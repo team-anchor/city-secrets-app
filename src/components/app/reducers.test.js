def79732f4feef725089a2c4ea0c8cc92fb661f7
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-18 13:29:20 -0700

    Write reducer test for load start

--- src/components/app/reducers.test.js
+++ src/components/app/reducers.test.js
@@ -1,8 +1,13 @@
-import { loading } from './reducers';
+import { loading, LOAD_START } from './reducers';
 
 describe('Loading reducer', () => {
   it('loads initial state', () => {
     const state = loading(undefined, {});
     expect(state).toBe(false);
+  });
+
+  it('starts load', () => {
+    const state = loading(false, { type: LOAD_START });
+    expect(state).toBe(true);
   });
 });

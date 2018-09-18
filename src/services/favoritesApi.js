e01f6d57c63ac2d71cdb1bd8cb1ec2382399d33a
Mario Quintana <pyrros2000@gmail.com>
2018-09-18 13:42:47 -0700

    Created favorites, favorite components and favoriteApi

---
+++ src/services/favoritesApi.js
@@ -0,0 +1,35 @@
+import { post, get, del } from './request';
+
+const URL = '/api';
+const FAVORITES_URL = `${URL}/favorites`;
+
+const getFavoriteUrl = FAVORITES_URL;
+
+export const addFavorite = ({ id, title, featured_image, location, description }) => {
+  const url = getFavoriteUrl(id);
+  return post(url, {
+    id,
+    title,
+    featured_image,
+    location,
+    description
+  });
+};
+
+export const getFavorites = () => {
+  return get(`${FAVORITES_URL}.json`)
+    .then(res => {
+      return Object.keys(res)
+        .map(key => res[key]);
+    });
+};
+
+export const getFavorite = id => {
+  const url = getFavoriteUrl(id);
+  return get(url);
+};
+
+export const removeFavorite = id => {
+  const url = getFavoriteUrl(id);
+  return del(url);
+};

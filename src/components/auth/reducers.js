b434e39fd4a57ff04efe23c1eb4920004898302c
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-18 14:22:46 -0700

    Write user reducer for checking auth

--- src/components/auth/reducers.js
+++ src/components/auth/reducers.js
@@ -14,3 +14,12 @@ export function user(state = null, { type, payload }) {
       return state;
   }
 }
+
+export function checkedAuth(state = false, { type }) {
+  switch(type) {
+    case CHECKED_AUTH:
+      return true;
+    default:
+      return state;
+  }
+}

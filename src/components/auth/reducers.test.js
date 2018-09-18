f82f58b8fbe00539aa571e9d6d9ea74af8c9554e
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-18 14:18:47 -0700

    Add auth reducer test for loading user and verify it will fail

--- src/components/auth/reducers.test.js
+++ src/components/auth/reducers.test.js
@@ -1,8 +1,14 @@
-import { user } from './reducers';
+import { user, USER_AUTH } from './reducers';
 
 describe('User reducer', () => {
   it('initializes to null', () => {
     const state = user(undefined, {});
     expect(state).toBeNull();
+  });
+
+  it('loads a user', () => {
+    const data = { name: 'user' };
+    const state = user(null, { type: USER_AUTH, payload: data });
+    expect(state).toEqual(data);
   });
 });

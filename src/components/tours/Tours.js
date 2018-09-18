1e5fa6e10eb341e5439613d913fb04c7efc173f8
Mario Quintana <pyrros2000@gmail.com>
2018-09-18 13:07:09 -0700

    Tours component created but no data fetched

---
+++ src/components/tours/Tours.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Tour from './Tour';
+import styles from './Tours.css';
+
+export default class Tours extends Component {
+
+  static propTypes = {
+    tours: PropTypes.arrayOf(Object)
+  };
+
+  render() {
+    const { tours } = this.props;
+
+    return (
+      <ul className={styles.tours}>
+        {tours.map((tour, i) => (
+          <Tour key={i} tour={tour}/>
+        ))}
+      </ul>
+    );
+  }
+}

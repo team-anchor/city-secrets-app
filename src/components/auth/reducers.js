c88c0f8d9341813ce75c5f387dd9db9ef20d6ef8
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-18 14:19:28 -0700

    Add user reducer case for loading user and verify test will pass

--- src/components/auth/reducers.js
+++ src/components/auth/reducers.js
@@ -2,8 +2,10 @@ export const USER_AUTH = 'USER_AUTH';
 export const LOGOUT = 'LOGOUT';
 export const CHECKED_AUTH = 'CHECKED_AUTH';
 
-export function user(state = null, { type }) {
+export function user(state = null, { type, payload }) {
   switch(type) {
+    case USER_AUTH:
+      return payload;
     default:
       return state;
   }

0bd1f7c7a2b12a12081b6fb89eb376b3fdc30a90
Mark Myers <markalope@gmail.com>
2018-09-19 15:05:16 -0700

    finalized the CSS for tours

--- src/components/tours/Tour.js
+++ src/components/tours/Tour.js
@@ -6,17 +6,19 @@ class Tour extends Component {
   render() { 
     return ( 
       <div className={styles.Tour}>
-        <div className="tourtile">
-          <div className="star">
-            <i className="far fa-star fa-2x"></i>
+        < a href="#"> 
+          <div className="tourtile">
+            <div className="star">
+              <i className="far fa-star fa-2x"></i>
+            </div>
+            <img className="cover" src="http://www.worldwidewalrusweb.com/bc2/europe_2.jpg" />
+            <div className="textcontainer">
+              <h2 className="tourhead">Secret Walking Tour</h2>
+              <p className="tour-tile-text">Here is the description of this tour. It is super cool, you should really check it out</p>
+            </div>
+            
           </div>
-          <img className="cover" src="http://www.worldwidewalrusweb.com/bc2/europe_2.jpg" />
-          <div className="textcontainer">
-            <h2 className="tourhead">Secret Walking Tour</h2>
-            <p className="tour-tile-text">Here is the description of this tour. It is super cool, you should really check it out</p>
-          </div>
-          
-        </div>
+        </a>
       </div>
 
     );

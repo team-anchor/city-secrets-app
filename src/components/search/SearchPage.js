cb27570283ad6d26ebb867f0a66728fe435944ba
Mark Myers <markalope@gmail.com>
2018-09-20 09:25:57 -0700

    creacted SearchPage

---
+++ src/components/search/SearchPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Search from './Search';
+
+class SearchPage extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        <Search />
+      </div>
+
+    );
+  }
+}
+ 
+export default SearchPage;
